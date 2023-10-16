4d06c6b17eea5838563c305f17f5ba960a91a095
maxime67 <maxime.heim3@gmail.com>
2023-10-16 22:33:24 +0200

    16/10

---
+++ src/Controller/ListImageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Repository\ImageRepository;
+use App\Repository\CaliberRepository;
+
+class ListImageController extends AbstractController
+{
+    #[Route('/list/image/{weapon_id}', name: 'app_list_image')]
+    public function index(Int $weapon_id,CaliberRepository $caliberRepository, ImageRepository $imageRepository): Response
+    {
+        return $this->render('list_image/index.html.twig', [
+            'ImagesList' => $imageRepository->findBy( ['weapon' => $weapon_id]),
+            'caliber_list' => $caliberRepository->findAll(),
+        ]);
+    }
+}
