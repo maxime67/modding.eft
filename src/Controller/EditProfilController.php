b569d01cbff2745fceac8facf6f70bed58d0c8a6
Maxime HEIM <maxime.heim2023@campus-eni.fr>
2023-11-06 16:19:17 +0100

    allow user to change username/completion user registery form

---
+++ src/Controller/EditProfilController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\EditProfilType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class EditProfilController extends AbstractController
+{
+    #[Route('/edit/profil', name: 'app_edit_profil')]
+    public function index(Request $request,EntityManagerInterface $entityManager): Response
+    {
+
+        $user = $this->getUser();
+
+        $form = $this->createForm(EditProfilType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            try{
+                $user->setUuid($form->get("uuid")->getData());
+            } catch(\Exception $e){
+                $e->getMessage();
+            }
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // ... perform some action, such as saving the task to the database
+            return $this->redirectToRoute('app_home');
+        }
+//        dd($form->createView());
+        return $this->render('edit_profil/index.html.twig', [
+            'currentUser' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+}
